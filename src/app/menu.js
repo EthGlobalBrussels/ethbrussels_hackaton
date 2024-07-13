96d03805b98544ebb94ec2810878b376d25eb07c
larfef <kerstensreda@outlook.com>
2024-07-13 22:25:33 +0200

    Resolve display issue

--- src/app/menu.js
+++ src/app/menu.js
@@ -1,16 +1,10 @@
 'use client'
 
-import { useUser } from '@auth0/nextjs-auth0/client';
 import menu from './menu.module.css';
 import Link from 'next/link';
 import ConnectWallet from './userData/connectWallet';
 
 export default function Menu() {
-    const { user, error, isLoading } = useUser();
-
-    // if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>{error.message}</div>;
-
     return (
       <div>
         <div className='fade-in' >

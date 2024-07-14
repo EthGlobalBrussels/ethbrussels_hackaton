76b2ded800e830eaca1cd5038e72987bbfdb4d77
larfef <kerstensreda@outlook.com>
2024-07-14 05:00:04 +0200

    Add refund

--- src/app/transaction/getWinners.js
+++ src/app/transaction/getWinners.js
@@ -2,7 +2,7 @@
 import Web3 from 'web3';
 import abi from '../abis/contractAbi.json';
 
-export async function GetWinners(matchId, team) {
+export async function GetWinners(_matchId, _team) {
   const contractAddress = '0x02a454Cc4068F0cc093c22544989a6a79BC27E54';
 
   const web3 = new Web3(window.ethereum);
@@ -14,7 +14,9 @@ export async function GetWinners(matchId, team) {
 
   const contract = new web3.eth.Contract(abi, contractAddress);
 
-  const result = await contract.methods.CountWinners(matchId, team);
+  const result = await contract.methods.CountWinners(_matchId, _team).send({
+	from: userAddress
+  })
 
   console.log('Transaction successful:', result);
 }
